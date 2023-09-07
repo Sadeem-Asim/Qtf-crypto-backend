2632dd44b39804c0a321d0a6e99f73b006550355
Sadeem Asim <hassan-rasheed>
2023-09-08 03:34:44 +0500

    leverage

--- sockets/binanceBtcLib.js
+++ sockets/binanceBtcLib.js
@@ -21,7 +21,7 @@ export default function binanceLib() {
     // console.log(data.markPrice);
     const { markPrice } = data;
     // console.log(markPrice);
-    // await leverage({ markPrice });
+    await leverage({ markPrice });
   });
   const logger = {
     ...DefaultLogger,
